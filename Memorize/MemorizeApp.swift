53bfa5aefba75678dcb2a179e51e266c46bbfcd5
atbee <athibet.pi@gmail.com>
2023-12-09 17:14:58 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Athibet Prawane on 9/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
