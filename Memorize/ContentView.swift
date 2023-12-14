ccc64e30100d5156c2d782eccd41bb50b9fe58fe
atbee <athibet.pi@gmail.com>
2023-12-15 06:35:31 +0700

    Use var to remove replicated code

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -24,15 +24,13 @@ struct CardView: View {
 
     var body: some View {
         ZStack {
+            var base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
-                Text("👻")
-                    .font(.largeTitle)
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base
             }
         }
     }
