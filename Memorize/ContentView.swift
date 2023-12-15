a8171d7a077cc990bb54b2012721261789538b40
atbee <athibet.pi@gmail.com>
2023-12-15 22:25:30 +0700

    Infer the variable types by remove the type

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -20,11 +20,11 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    var isFaceUp = false
 
     var body: some View {
         ZStack {
-            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
                 base.foregroundColor(.white)
                 base.strokeBorder(lineWidth: 2)
