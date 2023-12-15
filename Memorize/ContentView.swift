2af4d235d243271efafb209d011a6a600c6b6eb5
atbee <athibet.pi@gmail.com>
2023-12-15 22:17:49 +0700

    Use let for constant variable

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -24,7 +24,7 @@ struct CardView: View {
 
     var body: some View {
         ZStack {
-            var base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
                 base.foregroundColor(.white)
                 base.strokeBorder(lineWidth: 2)
